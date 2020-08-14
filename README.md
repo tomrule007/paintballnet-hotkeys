0c4fe15ab38acac4cdec1f0eb256591843372f6e
Thomas Herzog <Tomrule007@gmail.com>
2020-08-13 22:31:35 -0700

    Initial commit (first working version)

---
+++ README.md
@@ -0,0 +1,60 @@
+# paintballnet-hotkeys
+
+Bookmarklet that allows users to create hotkeys for paintballnet.net",
+
+---
+
+## Current hot keys
+
+| KEY     | COMMAND                       |
+| ------- | ----------------------------- |
+| r       | reload                        |
+| e       | get flag & get token          |
+| q       | swap free gun & semi-auto gun |
+| shift+E | sell flag & sell token        |
+| shift+R | game ready auto               |
+| shift+Q | game ready standby            |
+
+## Using Bookmarklet
+
+\*Link to current build [bookmarklet page](https://github.com/tomrule007/paintballnet-hotkeys/build/index.html)
+
+1. Open [http://paintballnet.net/play](http://paintballnet.net/play)
+2. Click `PBN-Hotkeys` bookmark
+
+\*Must click on the bookmarklet before connecting to the server.
+
+---
+
+## Development - Getting Started
+
+### Setup
+
+```bash
+$git clone https://github.com/tomrule007/paintballnet-hotkeys.git
+$cd paintballnet-hotkeys
+$npm install
+```
+
+### Generate bookmarklet
+
+```bash
+$npm run build
+```
+
+Open --->`build/index.html` & add bookmarklet to bookmark bar.
+
+### To change hotkeys
+
+Modify `src/index.js` and redo `Generate bookmarklet` instructions
+
+---
+
+## Future features
+
+- add status label to show hotkeys are working
+- add custom hotkey configure menu
+
+## Version History
+
+- 1.0.0: First version with hard bound hotkeys
---
+++ src/index.js
@@ -0,0 +1,79 @@
+// ==Bookmarklet==
+// @name PBN-Hotkeys
+// @author Thomas Herzog
+// ==/Bookmarklet==
+// proxy the window.WebSocket object
+var WebSocketProxy = new Proxy(window.WebSocket, {
+  construct: function (target, args) {
+    // create WebSocket instance
+    const instance = new target(...args);
+
+    // WebSocket "onopen" handler
+    const openHandler = (event) => {
+      console.log('Open', event);
+    };
+
+    // WebSocket "onmessage" handler
+    const messageHandler = (event) => {
+      console.log('Message', event);
+    };
+
+    // WebSocket "onclose" handler
+    const closeHandler = (event) => {
+      console.log('Close', event);
+      // remove event listeners
+      instance.removeEventListener('open', openHandler);
+      instance.removeEventListener('message', messageHandler);
+      instance.removeEventListener('close', closeHandler);
+    };
+
+    // add event listeners
+    instance.addEventListener('open', openHandler);
+    instance.addEventListener('message', messageHandler);
+    instance.addEventListener('close', closeHandler);
+
+    // proxy the WebSocket.send() function
+    const sendProxy = new Proxy(instance.send, {
+      apply: function (target, thisArg, args) {
+        console.log('target', target, 'thisArg', thisArg, 'Send', args);
+        target.apply(thisArg, args);
+      },
+    });
+
+    // replace the native send function with the proxy
+    instance.send = sendProxy;
+    window.sendPortal = instance.send.bind(instance);
+    // return the WebSocket instance
+    return instance;
+  },
+});
+
+// replace the native WebSocket with the proxy
+window.WebSocket = WebSocketProxy;
+function keydown({ key, shiftKey, altKey, ctrlKey, metaKey }) {
+  const modifierKey = '' + +shiftKey + +altKey + +ctrlKey + +metaKey;
+
+  switch (key + modifierKey) {
+    case 'E1000': // shift+E
+      sendPortal('{"id":"05","text":"1"}');
+      break;
+    case 'e0000': // e
+      sendPortal('{"id":"05","text":"1"}');
+      break;
+    case 'R1000': // shift+R
+      sendPortal('{"id":"05","text":"game ready auto"}');
+      break;
+    case 'r0000': // r
+      sendPortal('{"id":"05","text":"reload"}');
+      break;
+    case 'q0000': // q
+      sendPortal('{"id":"05","text":"swap & get gun back & put %lhand back"}');
+      break;
+    case 'Q1000': // shift+Q
+      sendPortal('{"id":"05","text":"game ready standby"}');
+      break;
+    default:
+      break;
+  }
+}
+document.addEventListener('keydown', keydown);
