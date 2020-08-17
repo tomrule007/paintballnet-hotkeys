d4f701977b75e756170cd79096fe402e450f5488
Thomas Herzog <Tomrule007@gmail.com>
2020-08-16 22:06:48 -0700

    Create PBN-Hotkey link

--- src/index.js
+++ src/index.js
@@ -77,3 +77,24 @@ function keydown({ key, shiftKey, altKey, ctrlKey, metaKey }) {
   }
 }
 document.addEventListener('keydown', keydown);
+
+function createUI() {
+  //Menu link
+  const a = document.createElement('a');
+  const linkText = document.createTextNode('PBN-Hotkeys');
+  a.appendChild(linkText);
+  a.title = 'hotkeys by tomrule007';
+  a.id = 'pbnHotkeysLink';
+  a.style.cssText = `position: fixed; 
+                     z-index: 1000;
+                     top: 0;
+                     right: 0;
+                     color: red;
+                     text-decoration: underline;
+                     cursor: pointer;
+                     `;
+
+  document.body.appendChild(a);
+
+}
+createUI();
