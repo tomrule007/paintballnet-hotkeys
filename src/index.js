9d853b15c94bf337c04d034a37edb42ea513b834
Thomas Herzog <Tomrule007@gmail.com>
2020-08-17 15:58:23 -0700

    Replace hotkey alias with actual commands

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@
 
 const savedHotkeys = {
   // key shift alt ctrl meta : cmd or alias (no slash needed)
-  E1000: '1', // shift+E
-  e0000: '2', // e
+  E1000: 'get token jacket & sell token & get flag jacket & sell flag', // shift+E
+  e0000: 'get token & get flag & put token jacket & put flag jacket', // e
   R1000: 'game ready auto', // shift+R
   r0000: 'reload', // r
   q0000: 'swap & get gun back & put %lhand back', // q
