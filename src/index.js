d16e0d03f2663fed8e826835e41dc573013fec0d
Thomas Herzog <Tomrule007@gmail.com>
2020-08-18 19:03:43 -0700

    Extract hotkeys into seperate file

--- src/index.js
+++ src/index.js
@@ -2,17 +2,8 @@
 // @name PBN-Hotkeys
 // @author Thomas Herzog
 // ==/Bookmarklet==
-// proxy the window.WebSocket object
 
-const savedHotkeys = {
-  // key shift alt ctrl meta : cmd or alias (no slash needed)
-  E1000: 'get token jacket & sell token & get flag jacket & sell flag', // shift+E
-  e0000: 'get token & get flag & put token jacket & put flag jacket', // e
-  R1000: 'game ready auto', // shift+R
-  r0000: 'reload', // r
-  q0000: 'swap & get gun back & put %lhand back', // q
-  Q1000: 'game ready standby', // shift+Q
-};
+import { savedHotkeys } from './savedHotkeys';
 
 function createWebSocketProxy() {
   var WebSocketProxy = new Proxy(window.WebSocket, {
---
+++ src/savedHotkeys.js
@@ -0,0 +1,9 @@
+export const savedHotkeys = {
+  // key shift alt ctrl meta : cmd or alias (no slash needed)
+  E1000: 'get token jacket & sell token & get flag jacket & sell flag', // shift+E
+  e0000: 'get token & get flag & put token jacket & put flag jacket', // e
+  R1000: 'game ready auto', // shift+R
+  r0000: 'reload', // r
+  q0000: 'swap & get gun back & put %lhand back', // q
+  Q1000: 'game ready standby', // shift+Q
+};
