c5fed4148d0dcd86d3ad1052d8a1d9edddb423f8
Thomas Herzog <Tomrule007@gmail.com>
2020-08-22 23:15:43 -0700

    Use default hotkeys on first load

--- src/savedHotkeys.js
+++ src/savedHotkeys.js
@@ -1,13 +1,13 @@
-export let savedHotkeys = JSON.parse(
-  window.localStorage.getItem('pbn-hotkeys') || '{}'
-);
+const defaultHotkeys = JSON.stringify({
+  // key shift alt ctrl meta : cmd or alias (no slash needed)
+  E1000: 'get token jacket & sell token & get flag jacket & sell flag', // shift+E
+  e0000: 'get token & get flag & put token jacket & put flag jacket', // e
+  R1000: 'game ready auto', // shift+R
+  r0000: 'reload', // r
+  q0000: 'swap & get gun back & put %lhand back', // q
+  Q1000: 'game ready standby', // shift+Q
+});
 
-// {
-//   // key shift alt ctrl meta : cmd or alias (no slash needed)
-//   E1000: 'get token jacket & sell token & get flag jacket & sell flag', // shift+E
-//   e0000: 'get token & get flag & put token jacket & put flag jacket', // e
-//   R1000: 'game ready auto', // shift+R
-//   r0000: 'reload', // r
-//   q0000: 'swap & get gun back & put %lhand back', // q
-//   Q1000: 'game ready standby', // shift+Q
-// };
+export let savedHotkeys = JSON.parse(
+  window.localStorage.getItem('pbn-hotkeys') || defaultHotkeys
+);
