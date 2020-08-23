5ecccb0ee8277915cfffd95b678fbcb0f55fcabd
Thomas Herzog <Tomrule007@gmail.com>
2020-08-22 22:45:44 -0700

    Use localstore hotkeys on load.

--- src/savedHotkeys.js
+++ src/savedHotkeys.js
@@ -1,9 +1,13 @@
-export const savedHotkeys = {
-  // key shift alt ctrl meta : cmd or alias (no slash needed)
-  E1000: 'get token jacket & sell token & get flag jacket & sell flag', // shift+E
-  e0000: 'get token & get flag & put token jacket & put flag jacket', // e
-  R1000: 'game ready auto', // shift+R
-  r0000: 'reload', // r
-  q0000: 'swap & get gun back & put %lhand back', // q
-  Q1000: 'game ready standby', // shift+Q
-};
+export let savedHotkeys = JSON.parse(
+  window.localStorage.getItem('pbn-hotkeys') || '{}'
+);
+
+// {
+//   // key shift alt ctrl meta : cmd or alias (no slash needed)
+//   E1000: 'get token jacket & sell token & get flag jacket & sell flag', // shift+E
+//   e0000: 'get token & get flag & put token jacket & put flag jacket', // e
+//   R1000: 'game ready auto', // shift+R
+//   r0000: 'reload', // r
+//   q0000: 'swap & get gun back & put %lhand back', // q
+//   Q1000: 'game ready standby', // shift+Q
+// };
