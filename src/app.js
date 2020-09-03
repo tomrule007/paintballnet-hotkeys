97433a27eea001363bda6e28c6761766150776ab
Thomas Herzog <Tomrule007@gmail.com>
2020-09-02 21:40:40 -0700

    Add double loading prevention mechanism

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ import Controller from './controller';
 import View from './view';
 import Model from './model';
 
-import { createWebSocketProxy } from './webSocketProxy';
-const view = new View();
-const model = new Model();
-const controller = new Controller(view, model);
+// Prevent double loading bookmarklet
+if (!window.pbnHotkeysLoaded) {
+  window.pbnHotkeysLoaded = true;
 
-console.log('PBN-Hotkeys by tomrule007');
+  const view = new View();
+  const model = new Model();
+  const controller = new Controller(view, model);
+
+  console.log('PBN-Hotkeys by tomrule007');
+}
