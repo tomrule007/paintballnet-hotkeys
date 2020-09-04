cd8eaf509ba6e939203e8ae157039f66fbc23e8f
Thomas Herzog <Tomrule007@gmail.com>
2020-09-03 23:27:01 -0700

    Add new view tests and update snapshot

--- src/__tests__/view.test.js
+++ src/__tests__/view.test.js
@@ -2,7 +2,7 @@ import View from '../view';
 import { screen } from '@testing-library/dom';
 import '@testing-library/jest-dom/extend-expect';
 
-describe('CreateUI()', () => {
+describe('View', () => {
   beforeEach(() => {
     // Set up blank mock dom
     document.body.innerHTML = '<div> </div>';
@@ -11,11 +11,11 @@ describe('CreateUI()', () => {
     const view = new View();
     // view.createUI();
   });
-  it('Generates UI to matches snapshot', async () => {
+  it('Initializes UI to matches snapshot', async () => {
     //make snapshot of modified dom
     expect(document.body).toMatchSnapshot();
   });
-  it('clicking hotkeys link shows menu', async () => {
+  it("clicking 'PBN-Hotkeys' link shows the hotkey menu", async () => {
     const menuDiv = document.getElementById('pbnHotkeysMenu');
 
     expect(menuDiv).not.toBeVisible();
@@ -24,5 +24,19 @@ describe('CreateUI()', () => {
     hotkeyLink.click();
 
     expect(menuDiv).toBeVisible();
+  });
+  it("clicking close button '[x]' hides the hotkey menu ", async () => {
+    //show the menu first and make sure that works
+    const menuDiv = document.getElementById('pbnHotkeysMenu');
+    expect(menuDiv).not.toBeVisible();
+
+    const hotkeyLink = screen.getByText(/hotkeys$/i);
+    hotkeyLink.click();
+
+    expect(menuDiv).toBeVisible();
+    // find close button and check that it hides the menu.
+    const closeButton = screen.getByText(/x$/i);
+    closeButton.click();
+    expect(menuDiv).not.toBeVisible();
   });
 });
