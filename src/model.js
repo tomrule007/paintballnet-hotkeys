c9b2d4bcf71adff6e3508fd6afc241c38d058221
Thomas Herzog <Tomrule007@gmail.com>
2020-09-04 18:03:38 -0700

    Remove default hotkeys

--- src/model.js
+++ src/model.js
@@ -1,15 +1,6 @@
-const defaultHotkeys = JSON.stringify({
-  // key shift alt ctrl meta : cmd or alias (no slash needed)
-  E100: 'get token jacket & sell token & get flag jacket & sell flag', // shift+E
-  e000: 'get token & get flag & put token jacket & put flag jacket', // e
-  R100: 'game ready auto', // shift+R
-  r000: 'reload', // r
-  q000: 'swap & get gun back & put %lhand back', // q
-  Q100: 'game ready standby', // shift+Q
-});
-
+// Todo: move saveHotkeys variable to Model class and update dependencies
 export let savedHotkeys = JSON.parse(
-  window.localStorage.getItem('pbn-hotkeys') || defaultHotkeys
+  window.localStorage.getItem('pbn-hotkeys') || JSON.stringify({})
 );
 export default class Model {
   constructor() {
