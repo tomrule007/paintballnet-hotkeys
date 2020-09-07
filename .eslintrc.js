7c3397934fbf75b1ab5cfe9c569263dc0b152cde
Thomas Herzog <Tomrule007@gmail.com>
2020-09-06 21:00:22 -0700

    Add prettier and link to eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,13 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard'
-  ],
+  extends: ['prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  rules: {
-  }
-}
+  rules: { 'prettier/prettier': ['error'] },
+};
