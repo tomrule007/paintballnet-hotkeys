b0df3437446df6b9333fb789784ac16b63221375
Thomas Herzog <Tomrule007@gmail.com>
2020-09-07 17:33:56 -0700

    Feature: Redirect to paintballnet on first click.
    Will not redirect if you are already on paintballnet.
    Also will not redirect if you are on the github repo or local host.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import app from './app';
 
 if (process.env.NODE_ENV === 'development') require('./paintballnet.css');
 
+if (process.env.NODE_ENV === 'production') {
+  if (
+    window.location.href !== 'http://paintballnet.net/play/' &&
+    window.location.href !==
+      'https://tomrule007.github.io/paintballnet-hotkeys/build/index.html' &&
+    window.location.hostname !== '127.0.0.1'
+  )
+    window.location.href = 'http://paintballnet.net/play/';
+}
 app();
