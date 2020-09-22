ccacf0046fe76a9f6c097852f492a5eabe95abd3
Thomas Herzog <Tomrule007@gmail.com>
2020-09-22 14:35:38 -0700

    Export all inline style to css file

--- src/template.js
+++ src/template.js
@@ -1,19 +1,14 @@
+import style from './template.module.css';
+
 function createUI() {
   // TODO: convert to template literal
   // Menu link
   const menuLinkDiv = document.createElement('div');
   menuLinkDiv.classList.add(
     'TPBTOverlayScrollItem',
-    'TPBTOverlayScrollInfoItem'
+    'TPBTOverlayScrollInfoItem',
+    style.pbnMenuLink
   );
-  menuLinkDiv.style.cssText = `position: fixed; 
-                     z-index: 1000;
-                     top: 2px;
-                     right: 2px;
-                     color: red;
-                     text-decoration: underline;
-                     cursor: pointer;
-                     `;
   const a = document.createElement('a');
   a.appendChild(document.createTextNode('PBN-Hotkeys'));
   a.title = `red = not connected
@@ -28,14 +23,14 @@ hotkeys by tomrule007`;
   menuDiv.classList.add(
     'TW3Panel',
     'TW3ContainerBorder',
-    'TW3ContainerBackground'
+    'TW3ContainerBackground',
+    style.pbnMenu,
+    style.hidden
   );
-  menuDiv.style.cssText =
-    'z-index: 8; right: 2px; top: 40px; position: fixed; display: none;';
 
   // menu label
   const menuBarDiv = document.createElement('div');
-  menuBarDiv.style.cssText = 'display: flex; justify-content:	space-between;';
+  menuBarDiv.classList.add(style.pbnMenuBar);
   const menuLabelEl = document.createElement('span');
   menuLabelEl.append(
     document.createTextNode('Paintballnet-Hotkeys by tomrule007')
@@ -45,18 +40,16 @@ hotkeys by tomrule007`;
   // menu content container
   const menuContentEl = document.createElement('div');
   menuContentEl.id = 'pbnHotkeyMenuDisplay';
-  menuContentEl.classList.add('TPBTListBox');
-  menuContentEl.style.cssText =
-    'margin: 5px 2px; font-family: Lucida Console; font-size: 10pt; padding: 2px;';
+  menuContentEl.classList.add('TPBTListBox', style.pbnMenuContent);
   // menu close button
   const closeButtonEl = document.createElement('button');
   closeButtonEl.append(document.createTextNode('X'));
   closeButtonEl.classList.add(
     'TW3Button',
     'TW3ButtonBackground',
-    'TW3ButtonBorder'
+    'TW3ButtonBorder',
+    style.pbnMenuButton
   );
-  closeButtonEl.style.cssText = 'margin: 2px; font-size: 8pt;';
   closeButtonEl.id = 'pbnHotkeysMenuCloseButton';
   // save changes button
   const saveButton = document.createElement('button');
@@ -64,14 +57,13 @@ hotkeys by tomrule007`;
   saveButton.classList.add(
     'TW3Button',
     'TW3ButtonBackground',
-    'TW3ButtonBorder'
+    'TW3ButtonBorder',
+    style.pbnMenuButton
   );
-  saveButton.style.cssText = 'margin: 2px; font-size: 8pt;';
   saveButton.id = 'pbnHotkeysMenuSaveButton';
 
   const saveButtonContainerEl = document.createElement('div');
-  saveButtonContainerEl.style.display = 'flex';
-  saveButtonContainerEl.style.justifyContent = 'center';
+  saveButtonContainerEl.classList.add(style.pbnMenuFooter);
 
   menuBarDiv.append(menuLabelEl, closeButtonEl);
   saveButtonContainerEl.append(saveButton);
@@ -83,11 +75,9 @@ hotkeys by tomrule007`;
 function createHotkeyCard(hotkey, hotkeyText, command) {
   // TODO: convert to template literal
   const hotkeyCard = document.createElement('div');
-  hotkeyCard.classList.add('pbnHotkeyCard');
-  hotkeyCard.style.position = 'relative';
+  hotkeyCard.classList.add(style.pbnHotkeyCard);
   const hotkeyDivEl = document.createElement('div');
-  hotkeyDivEl.style.cssText =
-    'display: inline-block; min-width: 60px; textAlign: right; margin: 0px 2px';
+  hotkeyDivEl.classList.add(style.pbnHotkeyDiv);
 
   const hotkeyEl = document.createElement('a');
   hotkeyEl.dataset.hotkeyCode = hotkey;
@@ -97,10 +87,10 @@ function createHotkeyCard(hotkey, hotkeyText, command) {
   hotkeyCommandInput.classList.add(
     'TW3EditBox',
     'TW3EditBorder',
-    'TW3EditBackground'
+    'TW3EditBackground',
+    style.pbnCommandInput
   );
   hotkeyCommandInput.value = command;
-  hotkeyCommandInput.style.width = '350px';
   hotkeyCommandInput.type = 'text';
   hotkeyCommandInput.setAttribute('placeholder', 'type commands here');
   hotkeyCommandInput.onkeydown = (e) => e.stopPropagation();
@@ -112,29 +102,27 @@ function createHotkeyCard(hotkey, hotkeyText, command) {
 function createHotkeyEditCard() {
   // TODO: convert to template literal
   const hotkeySetContainer = document.createElement('div');
-  hotkeySetContainer.style.cssText =
-    'display: flex; text-align: center; font-weight: bold; background: white;';
+  hotkeySetContainer.classList.add(style.pbnHotkeyEditCard);
   const hotkeySetText = document.createTextNode('New hotkey:');
   const hotkeyDiv = document.createElement('div');
-  hotkeyDiv.style.flex = '1 1';
+  hotkeyDiv.classList.add(style.pbnHotkeyEditCardEl);
   const setHotkeyButton = document.createElement('button');
   setHotkeyButton.append(document.createTextNode('Set'));
   setHotkeyButton.classList.add(
     'TW3Button',
     'TW3ButtonBackground',
-    'TW3ButtonBorder'
+    'TW3ButtonBorder',
+    style.pbnMenuButton
   );
-  setHotkeyButton.style.cssText = 'font-size: 8pt;';
 
   const deleteHotkeyButton = document.createElement('button');
   deleteHotkeyButton.append(document.createTextNode('Delete'));
   deleteHotkeyButton.classList.add(
     'TW3Button',
     'TW3ButtonBackground',
-    'TW3ButtonBorder'
+    'TW3ButtonBorder',
+    style.pbnMenuButton
   );
-  deleteHotkeyButton.style.cssText = 'font-size: 8pt;';
-  deleteHotkeyButton.style.margin = '0 2px';
 
   hotkeySetContainer.append(
     hotkeySetText,
@@ -148,23 +136,7 @@ function createHotkeyEditCard() {
 
 function createHUD() {
   const hud = document.createElement('div');
-  hud.setAttribute(
-    'style',
-    `position: absolute;
-    top: 0px;
-    left: 0px;
-    right: 0px;
-    height: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: center;
-    z-index: 20000000; 
-    pointer-events: none;
-    perspective: inherit;
-    transform-style: preserve-3d;
-    transform: translateZ(1px);`
-  );
+  hud.classList.add(style.pbnHud);
   document.body.append(hud);
   return hud;
 }
