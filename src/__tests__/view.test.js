150b3a78fe3b9ccad7cc57a52fb1a970ed1c6236
Thomas Herzog <Tomrule007@gmail.com>
2020-09-21 22:50:14 -0700

    Refactor view test: use view element references. Using the view objects internal references instead of ids.

--- src/__tests__/view.test.js
+++ src/__tests__/view.test.js
@@ -3,40 +3,37 @@ import { screen } from '@testing-library/dom';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('View', () => {
+  let view;
   beforeEach(() => {
     // Set up blank mock dom
     document.body.innerHTML = '<div> </div>';
 
     // Run UI generating code
-    const view = new View();
+    view = new View();
     // view.createUI();
   });
   it('Initializes UI to matches snapshot', async () => {
-    //make snapshot of modified dom
     expect(document.body).toMatchSnapshot();
   });
   it("clicking 'PBN-Hotkeys' link shows the hotkey menu", async () => {
-    const menuDiv = document.getElementById('pbnHotkeysMenu');
-
-    expect(menuDiv).not.toBeVisible();
+    expect(view.$menu).not.toBeVisible();
 
     const hotkeyLink = screen.getByText(/hotkeys$/i);
     hotkeyLink.click();
 
-    expect(menuDiv).toBeVisible();
+    expect(view.$menu).toBeVisible();
   });
   it("clicking close button '[x]' hides the hotkey menu ", async () => {
-    //show the menu first and make sure that works
-    const menuDiv = document.getElementById('pbnHotkeysMenu');
-    expect(menuDiv).not.toBeVisible();
+    // show the menu first and make sure that works
+    expect(view.$menu).not.toBeVisible();
 
     const hotkeyLink = screen.getByText(/hotkeys$/i);
     hotkeyLink.click();
 
-    expect(menuDiv).toBeVisible();
+    expect(view.$menu).toBeVisible();
     // find close button and check that it hides the menu.
     const closeButton = screen.getByText(/x$/i);
     closeButton.click();
-    expect(menuDiv).not.toBeVisible();
+    expect(view.$menu).not.toBeVisible();
   });
 });
