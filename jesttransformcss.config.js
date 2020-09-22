b5941fd780c13fdda867d9ab87665888568e1e2c
Thomas Herzog <Tomrule007@gmail.com>
2020-09-22 10:44:08 -0700

    Add support module and global css files

--- jesttransformcss.config.js
+++ jesttransformcss.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  modules: true,
+  modules: (filename) => filename.endsWith('.module.css'),
 };
--- webpack.common.js
+++ webpack.common.js
@@ -21,8 +21,23 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/i,
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
       },
     ],
   },
